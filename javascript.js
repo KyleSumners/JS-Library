3dbf167862955ae79d348b6baa75d50e12e4d8ac
KyleSumners <114194918+KyleSumners@users.noreply.github.com>
2024-03-18 22:53:57 -0500

    Add ability to add books to the library with console

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+const container = document.querySelector('.container');
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -12,4 +15,35 @@ function Book(title, author, pages, read) {
     }
     return result;
   }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
+}
+
+function displayBooks(myLibrary) {
+  myLibrary.forEach(item => {
+    const book = document.createElement('div.book');
+    // Create areas for book information
+    const title = document.createElement('div.title');
+    const readStatus = document.createElement('div.read');
+    const author = document.createElement('div.author');
+    const numPages = document.createElement('div.pages');
+    // Set all the necessary book information
+    title.textContent = item.title;
+    readStatus.textContent = item.read;
+    author.textContent = item.author;
+    numPages.textContent = item.pages;
+
+    console.log(item.author + " " + item.title);
+
+    book.style.display = 'grid'
+    book.style.gridTemplateRows = '4fr 1fr 1fr 1fr';
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(numPages);
+    book.appendChild(readStatus);
+
+    container.appendChild(book);
+  }); 
 }
